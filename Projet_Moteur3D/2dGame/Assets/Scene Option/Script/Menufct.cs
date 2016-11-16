a70593346a0cc1d4619572014a4d60e8dbed9807
Nurbain <nathan.urbain@etu.unistra.fr>
2016-11-16 20:35:47 +0100

    Ajout scène option + Asset graphique pixel

---
+++ Projet_Moteur3D/2dGame/Assets/Scene Option/Script/Menufct.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menufct : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void option()
+	{
+	
+	}
+
+	public void Credit()
+	{
+	
+	}
+
+	public void start()
+	{
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void quitter()
+	{
+		Application.Quit();
+	}
+}
---
+++ Projet_Moteur3D/2dGame/Assets/Scene Option/Script/hightlighted.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public class hightlighted : MonoBehaviour , IPointerEnterHandler , IPointerExitHandler{
+	// Use this for initialization
+	public GameObject droit;
+	public GameObject gauche;
+
+	void Start () {
+		droit.SetActive (false);
+		gauche.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+			droit.SetActive (true);
+			gauche.SetActive (true);
+	}
+
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		droit.SetActive (false);
+		gauche.SetActive (false);
+	}
+
+}
