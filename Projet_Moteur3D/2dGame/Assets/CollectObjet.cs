3181621e910b2a5bdaf27c0b6ea7fc1a218498ae
Nurbain <nathan.urbain@etu.unistra.fr>
2016-11-18 22:01:11 +0100

    Ajout script inventaire et mecanisme de collect

---
+++ Projet_Moteur3D/2dGame/Assets/CollectObjet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectObjet : MonoBehaviour {
+
+	public GameObject Objetscene;
+	public GameObject Objetinv;
+	// Use this for initialization
+	void Start () {
+		Objetinv.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			Objetscene.SetActive (false);
+			Objetinv.SetActive (true);
+		}
+	}
+}
