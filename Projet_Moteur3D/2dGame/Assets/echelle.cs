a2662e0f0d8ec0d0ed5d09379bb230da3d99ec10
Nurbain <nathan.urbain@etu.unistra.fr>
2016-11-27 21:21:18 +0100

    Ajout script et texture pour echelle

---
+++ Projet_Moteur3D/2dGame/Assets/echelle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class echelle : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if(Input.GetKey("up")) {
+			other.gameObject.transform.position += Vector3.up * (Time.deltaTime*3);
+		}
+
+		if(Input.GetKey("down")) {
+			other.gameObject.transform.position -= Vector3.up * (Time.deltaTime*2);
+		}
+	}
+
+}
