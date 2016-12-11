6b19611542a24af2f0ed720284efa9701e076870
llambrecht <lambrecht.louis@yahoo.fr>
2016-12-11 12:25:36 +0100

    Add spyke script and player damage / knockback

---
+++ Projet_Moteur3D/2dGame/Assets/Script/Game_mecanics/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//By Louis LAMBRECHT
+
+public class Player : MonoBehaviour {
+
+	public int currHealth;
+	public int maxHealth = 5;
+	
+	public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		currHealth = maxHealth;
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currHealth > maxHealth){
+			currHealth = maxHealth;
+		}
+		if(currHealth <= 0){
+			Die();
+		}
+	}
+
+	void Die(){
+		Application.LoadLevel(Application.loadedLevel);
+		
+	}
+	
+	void Damage(int n){
+		currHealth -= n;
+	}
+	
+	public IEnumerator Knockback float knockD, float knockPwr, Vector3 direction){
+		float timer = 0;
+		while(knockD > timer){
+			timer += Time.deltaTime;
+			rb2d.AddForce(new Vector3(direction.x * -100, direction.y knockPwr, transform.position.z));
+		}
+		
+		yield return 0;
+		
+	}
+	
+}
---
+++ Projet_Moteur3D/2dGame/Assets/Script/Game_mecanics/Spyke.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//By Louis LAMBRECHT
+
+public class Spyke : MonoBehaviour {
+
+	private Player p;
+
+	// Use this for initialization
+	void Start () {
+		p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.CompareTag("Player")){
+			p.Damage(1);
+			
+			StartCoroutine(player.Knockback(0.02f, 300, p.transform.position));
+			
+		}
+		
+		
+	}
+	
+}
