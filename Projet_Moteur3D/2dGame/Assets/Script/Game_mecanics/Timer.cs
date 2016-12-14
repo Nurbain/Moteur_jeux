42c49d8eec296a78fbdfd272a08df5e7382ceba4
Nurbain <nathan.urbain@etu.unistra.fr>
2016-12-15 00:57:39 +0100

    Ajout timer

---
+++ Projet_Moteur3D/2dGame/Assets/Script/Game_mecanics/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//By Nathan Urbain
+
+public class Timer : MonoBehaviour {
+
+	public static float temps = 0 ;
+	private string courant;
+
+	public Rect tps;
+	public GUISkin skin;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		temps = Time.time;
+		courant = string.Format ("{0:0.0}", temps);
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		GUI.Label (tps, courant , skin.GetStyle("Timer"));
+
+	}
+}
