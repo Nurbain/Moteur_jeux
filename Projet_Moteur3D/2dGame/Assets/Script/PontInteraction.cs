93c653fb826335c9123371b2f2297c27eb982c97
WENDLING Quentin <q.wendling@ad.unistra.fr>
2016-12-14 15:21:45 +0100

    Modif trucs qui clignote

--- Projet_Moteur3D/2dGame/Assets/Script/PontInteraction.cs
+++ Projet_Moteur3D/2dGame/Assets/Script/PontInteraction.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class PontInteraction : MonoBehaviour {
-
+	public GameObject elements;
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<CanvasGroup>().alpha = 0;
 		gameObject.GetComponent<BoxCollider2D> ().enabled = false;
+		elements.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -17,5 +18,6 @@ public class PontInteraction : MonoBehaviour {
 	public void OkGo(){
 		gameObject.GetComponent<CanvasGroup>().alpha = 1;
 		gameObject.GetComponent<BoxCollider2D> ().enabled = true;
+		elements.SetActive (true);
 	}
 }
